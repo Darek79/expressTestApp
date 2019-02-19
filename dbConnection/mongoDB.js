c555effc67afcd01c32110cecbaec9335e3139cb
Darek K <buzzer79@gmail.com>
2019-02-19 19:29:08 +0100

    Delete mongoDB.js

--- dbConnection/mongoDB.js
+++
@@ -1,4 +0,0 @@
-const {mongoose} = require('mongoose');
-
-mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb+srv://buzzer79:Kumar123@cluster0-lm2e5.gcp.mongodb.net/testENV?retryWrites=true`,{ useNewUrlParser: true })
