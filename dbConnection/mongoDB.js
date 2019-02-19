a36a6d5b47487a002226672de1d434a7d4d7052a
buzzer79 <buzzer79@gmail.com>
2019-02-19 13:23:18 -0500

    test2

--- dbConnection/mongoDB.js
+++ dbConnection/mongoDB.js
@@ -1,4 +1,4 @@
 const {mongoose} = require('mongoose');
 
 mongoose.Promise = global.Promise;
-
+mongoose.connect(`mongodb+srv://buzzer79:Kumar123@cluster0-lm2e5.gcp.mongodb.net/testENV?retryWrites=true`,{ useNewUrlParser: true })
